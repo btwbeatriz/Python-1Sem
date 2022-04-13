d0b13d0c59838df7b82178999a6cad4a28265e02
btwbeatriz <rm95246@fiap.com.br>
2022-04-13 12:16:46 -0300

    aula5

--- While.py
+++ While.py
@@ -1,40 +1,40 @@
-#n = int(input('Número: '))
-#cont = 1
+n = int(input('Número: '))
+cont = 1
 
-#while cont <= n:
-  #print('cont: ', cont)
-  #cont+=1
-
-
-
-
-#inicio = int(input('Início'))
-#fim = int(input('Fim: '))
-
-#while inicio <= fim:
-  #print('Contador:', inicio)
-  #inicio +=1
+while cont <= n:
+  print('cont: ', cont)
+  cont+=1
 
 
 
 
-#i = int(input('Digite um número: '))
-#f = int(input('Digite um número: '))
-#if i < f:
-  #print('Erro...')
-#else:  
-  #while i >= f:
-    #print('i: ', i)
-  #i -= 1
+inicio = int(input('Início'))
+fim = int(input('Fim: '))
+
+while inicio <= fim:
+  print('Contador:', inicio)
+  inicio +=1
 
 
-#n = int(input('Digite um número: '))
-#cont = 0
 
-#while cont < n*2:
-  #if cont%2 == 0:
-    #print('Cont: ', cont)
-  #cont+=1
+
+i = int(input('Digite um número: '))
+f = int(input('Digite um número: '))
+if i < f:
+  print('Erro...')
+else:  
+  while i >= f:
+    print('i: ', i)
+  i -= 1
+
+
+n = int(input('Digite um número: '))
+cont = 0
+
+while cont < n*2:
+  if cont%2 == 0:
+    print('Cont: ', cont)
+  cont+=1
 
 
 
@@ -51,9 +51,3 @@ while n1 >= n2:
     r = r + 1
 
 print(r)
-
-
-  
-
-
-    
